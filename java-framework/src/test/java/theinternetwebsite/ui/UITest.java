43ff4b00a3a96334268366dcfd621ed14bbbd430
Juan Pedro Sugg Gilbert <juanpedrosugg@outlook.com>
2022-11-24 05:03:33 -0300

    Testing new CI config

--- java-framework/src/test/java/theinternetwebsite/ui/UITest.java
+++ java-framework/src/test/java/theinternetwebsite/ui/UITest.java
@@ -92,7 +92,7 @@ public class UITest {
         chromeOptions.addArguments("--no-sandbox"); //
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-        capabilities.setCapability(CapabilityType.SUPPORTS_NETWORK_CONNECTION, true);
+        //capabilities.setCapability(CapabilityType.SUPPORTS_NETWORK_CONNECTION, true);
         capabilities.setCapability("se:recordVideo", true);
         capabilities.setCapability("se:timeZone", "US/Pacific");
         capabilities.setCapability("se:screenResolution", "1920x1080");
