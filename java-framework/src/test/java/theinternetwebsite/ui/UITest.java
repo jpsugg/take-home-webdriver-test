f72e7cafd3d2e4019179f2fcf98dc3e8dfba6b08
Juan Pedro Sugg Gilbert <juanpedrosugg@outlook.com>
2022-11-24 23:37:19 -0300

    Testing new CI conf

--- java-framework/src/test/java/theinternetwebsite/ui/UITest.java
+++ java-framework/src/test/java/theinternetwebsite/ui/UITest.java
@@ -16,9 +16,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.remote.LocalFileDetector;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.remote.LocalFileDetector;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
