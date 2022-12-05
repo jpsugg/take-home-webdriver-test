423e5d779ddf790145683144afa6ab5813f7acb3
Juan Pedro Sugg Gilbert <juanpedrosugg@outlook.com>
2022-12-05 01:45:54 -0300

    Testing new automerge config

--- java-framework/src/test/java/theinternetwebsite/ui/testcases/WindowsTest.java
+++ java-framework/src/test/java/theinternetwebsite/ui/testcases/WindowsTest.java
@@ -1,9 +1,9 @@
 package theinternetwebsite.ui.testcases;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 import theinternetwebsite.ui.UITest;
 import theinternetwebsite.ui.pageobjects.WindowsPage;
-import org.testng.annotations.Test;
 
 public class WindowsTest extends UITest {
 
