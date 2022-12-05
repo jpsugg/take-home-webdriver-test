483dde59d57533cc417e33941d562586b20d5a70
Juan Pedro Sugg Gilbert <juanpedrosugg@outlook.com>
2022-12-04 23:19:22 -0300

    Test automerge

--- java-framework/src/test/java/theinternetwebsite/ui/testcases/DownloadTest.java
+++ java-framework/src/test/java/theinternetwebsite/ui/testcases/DownloadTest.java
@@ -1,8 +1,8 @@
 package theinternetwebsite.ui.testcases;
 
+import org.testng.Assert;
 import theinternetwebsite.ui.UITest;
 import theinternetwebsite.ui.pageobjects.DownloadPage;
-import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
 
