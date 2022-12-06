d5f3d2188580611b258e59e9fae4da65dbc58e29
Juan Pedro Sugg Gilbert <juanpedrosugg@outlook.com>
2022-12-05 23:13:46 -0300

    Testing new merge config

--- java-framework/src/test/java/theinternetwebsite/ui/testcases/NotificationMessageRenderedTest.java
+++ java-framework/src/test/java/theinternetwebsite/ui/testcases/NotificationMessageRenderedTest.java
@@ -1,8 +1,8 @@
 package theinternetwebsite.ui.testcases;
 
+import org.testng.Assert;
 import theinternetwebsite.ui.UITest;
 import theinternetwebsite.ui.pageobjects.NotificationMessageRenderedPage;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NotificationMessageRenderedTest extends UITest {
