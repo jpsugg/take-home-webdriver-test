910eb12c8e5482c3da0f00d041159fdf5b490649
Juan Pedro Sugg Gilbert <juanpedrosugg@outlook.com>
2022-12-06 00:41:38 -0300

    Testing new merge config

--- java-framework/src/test/java/theinternetwebsite/ui/testcases/JavascriptErrorTest.java
+++ java-framework/src/test/java/theinternetwebsite/ui/testcases/JavascriptErrorTest.java
@@ -1,9 +1,9 @@
 package theinternetwebsite.ui.testcases;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 import theinternetwebsite.ui.UITest;
 import theinternetwebsite.ui.pageobjects.JavascriptErrorPage;
-import org.testng.annotations.Test;
 
 public class JavascriptErrorTest extends UITest {
 
