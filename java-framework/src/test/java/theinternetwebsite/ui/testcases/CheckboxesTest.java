07ddbf195f6d3aa4b99baacbad7eca7f524ae5b1
Juan Pedro Sugg Gilbert <juanpedrosugg@outlook.com>
2023-04-11 18:44:30 -0400

    Refactored CheckboxesTest and CheckboxesPage

--- java-framework/src/test/java/theinternetwebsite/ui/testcases/CheckboxesTest.java
+++ java-framework/src/test/java/theinternetwebsite/ui/testcases/CheckboxesTest.java
@@ -1,32 +1,40 @@
 package theinternetwebsite.ui.testcases;
 
 import org.testng.Assert;
-import java.util.stream.IntStream;
 import org.testng.annotations.Test;
 import theinternetwebsite.ui.UITest;
 import theinternetwebsite.ui.pageobjects.CheckboxesPage;
 
+import java.util.List;
+
 public class CheckboxesTest extends UITest {
-    public CheckboxesTest() { }
-    @Test(description="Checkboxes can be checked and unchecked")
-    public void checkboxChecking() {
-        CheckboxesPage checkboxesPage = new CheckboxesPage(this);
 
-        // Validate page loaded
-        Assert.assertTrue(checkboxesPage.isPageOpen(), "Page not open");
+    @Test(description = "Checkboxes can be checked and unchecked")
+    public void testCheckboxChecking() {
+        try {
+            CheckboxesPage checkboxesPage = new CheckboxesPage(this);
 
-        // All checkboxes toggled back and forth
-        IntStream.range(0, checkboxesPage.getCheckboxElements().size()).forEach(index-> {
-            Boolean prevStatus, currentStatus, expectedStatus;
-            // Toggle/ untoggle the checkbox
-            prevStatus = checkboxesPage.clickOnCheckbox(index);
-            currentStatus = checkboxesPage.getCheckboxCurrentValue(index);
-            Assert.assertNotEquals(prevStatus, currentStatus);
-            // Toggle/untoggle the checkbox back
-            expectedStatus = checkboxesPage.getCheckboxExpectedValue(index);
-            checkboxesPage.clickOnCheckbox(index);
-            currentStatus = checkboxesPage.getCheckboxCurrentValue(index);
-            Assert.assertEquals(currentStatus, expectedStatus);
-        });
+            // Validate page loaded
+            Assert.assertTrue(checkboxesPage.isPageOpen(), "Page not open");
+
+            // All checkboxes toggled back and forth
+            List<Boolean> currentStatuses = checkboxesPage.getAllCheckboxCurrentValues();
+            for (int i = 0; i < currentStatuses.size(); i++) {
+                // Toggle/untoggle the checkbox
+                boolean prevStatus = currentStatuses.get(i);
+                boolean isChecked = checkboxesPage.toggleCheckbox(i);
+                Boolean currentStatus = checkboxesPage.getCheckboxCurrentValue(i);
+                Assert.assertNotNull(currentStatus, "Checkbox value is null");
+                Assert.assertEquals(currentStatus.booleanValue(), isChecked);
+
+                // Toggle/untoggle the checkbox back
+                checkboxesPage.toggleCheckbox(i);
+                currentStatus = checkboxesPage.getCheckboxCurrentValue(i);
+                Assert.assertNotNull(currentStatus, "Checkbox value is null");
+                Assert.assertEquals(currentStatus.booleanValue(), prevStatus);
+            }
+        } catch (Exception e) {
+            Assert.fail("Test case failed due to exception: " + e.getMessage());
+        }
     }
 }
